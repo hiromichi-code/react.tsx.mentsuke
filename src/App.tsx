e6f49d88ed53c80705d4d032c002f62674fce017
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-13 13:03:37 +0900

    セマンティックUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import image from './images/book.svg';
+import Button from './components/Button';
+
+import './App.css';
 
 const App = () => {
   return(
     <div className="welcome">
-      <h1>mentsuke</h1>
-      <h2>shinohara Book Binding inc.</h2>
-      <h3>Fukuoka Japan</h3>
+      <h1 className="ui header">mentsuke</h1>
+      <h4 className="ui header">shinohara Book Binding inc.</h4>
+      <h5 className="ui header">Fukuoka Japan</h5>
+      <div className="btn">
+        <Button />
+      </div>
       <img src={image} alt="book"/>
+      <footer>&copy;Shinohara Book Binding inc.</footer>
     </div>
   );
 }
