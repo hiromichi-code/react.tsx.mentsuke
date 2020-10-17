db9bbb2b4c58925c38250a1f6e8e9b5f87f27613
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-17 15:12:41 +0900

    デザイン修正

--- src/App.tsx
+++ src/App.tsx
@@ -12,26 +12,27 @@ const App = () => {
     <div className="main">
       <div className="header">
         <Header />
+        <h3>Shinohara Book Binding inc.</h3>
+        <h5>Fukuoka Japan</h5>
       </div>
-      <h3>Shinohara Book Binding inc.</h3>
-      <h5>Fukuoka Japan</h5>
-      <div className="btn">
-        <button className="ui primary basic button">
-          <Link to="/Sixteen">16頁</Link>
-        </button>
-        <button className="ui primary basic button">
-          <Link to="/eight">8頁</Link>
-        </button>
-        <button className="ui primary basic button">
-          <Link to="/four">4頁</Link>
-        </button>
-        <button className="ui primary basic button">
-          <Link to="/two">2頁</Link>
-        </button>
-      </div>
-      <img src={image} alt="book" />
-      <div className="footer">
-        <Footer />
+      <div className="hero">
+        <div className="btn">
+          <button className="ui primary basic button">
+            <Link to="/Sixteen">16頁</Link>
+          </button>
+          <button className="ui primary basic button">
+            <Link to="/eight">8頁</Link>
+          </button>
+          <button className="ui primary basic button">
+            <Link to="/four">4頁</Link>
+          </button>
+          <button className="ui primary basic button">
+            <Link to="/two">2頁</Link>
+          </button>
+        </div>
+        <div className="footer">
+          <Footer />
+        </div>
       </div>
     </div>
   );
