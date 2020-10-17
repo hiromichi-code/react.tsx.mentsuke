dff82656df9f4b62cd9c12cd60e32fd870703dae
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-18 07:32:04 +0900

    image削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
-import image from "./images/book.svg";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
