77bd5a2c7e12b4f344037c8748a805c3a8d39429
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-18 09:33:58 +0900

    Routhing書き換え

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -1,23 +1,23 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import App from "./App";
 import Sixteen from "./Sixteen";
 import Eight from "./Eight";
 import Four from "./Four";
 import Two from "./Two";
 
-class Routing extends React.Component {
+export default class Routing extends React.Component {
   render() {
     return (
       <BrowserRouter>
-        <Route exact path="./App" component={App} />
-        <Route exact path="./Sixteen" component={Sixteen} />
-        <Route exact path="./Eight" component={Eight} />
-        <Route exact path="./Four" component={Four} />
-        <Route exact path="./Two" component={Two} />
+        <Switch>
+          <Route exact path="./App" component={App} />
+          <Route exact path="./Sixteen" component={Sixteen} />
+          <Route exact path="./Eight" component={Eight} />
+          <Route exact path="./Four" component={Four} />
+          <Route exact path="./Two" component={Two} />
+        </Switch>
       </BrowserRouter>
     );
   }
 }
-
-export default Routing;
