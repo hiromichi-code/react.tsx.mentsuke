0b60bf2369d6455d5be1b10b0e9b2f042f2fbb35
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-18 18:13:52 +0900

    Routing修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import "./App.css";
 
 const App = () => {
+  return (
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+    </BrowserRouter>
+  );
+};
+
+const Home = () => {
   return (
     <div className="main">
       <div className="header">
@@ -15,18 +23,10 @@ const App = () => {
       </div>
       <div className="hero">
         <div className="btn">
-          <button className="ui primary basic button">
-            <Link to="/Sixteen">16頁</Link>
-          </button>
-          <button className="ui primary basic button">
-            <Link to="/eight">8頁</Link>
-          </button>
-          <button className="ui primary basic button">
-            <Link to="/four">4頁</Link>
-          </button>
-          <button className="ui primary basic button">
-            <Link to="/two">2頁</Link>
-          </button>
+          <button className="ui primary basic button">16頁</button>
+          <button className="ui primary basic button">8頁</button>
+          <button className="ui primary basic button">4頁</button>
+          <button className="ui primary basic button">2頁</button>
         </div>
         <div className="footer">
           <Footer />
