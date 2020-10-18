7aceca5c5411ea89389c26b044d93d94db6ab15f
hiromichi-code <hiromichi.s.617@gmail.com>
2020-10-19 06:28:36 +0900

    諸々修正

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import "./App.css";
 const App = () => {
   return (
     <BrowserRouter>
-      <Route exact path="./" component={Home} />
+      <Route exact path="/" component={Home} />
       <Route exact path="./Sixteen" component={Sixteen} />
       <Route exact path="./Eight" component={Eight} />
       <Route exact path="./Four" component={Four} />
